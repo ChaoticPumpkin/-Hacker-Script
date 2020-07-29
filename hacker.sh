abef04c0d6cf03c83a6fbcf7e7ea63a3c8957550
ChaoticPumpkin <59938482+ChaoticPumpkin@users.noreply.github.com>
2020-07-28 20:34:10 -0700

    Update hacker.sh

--- hacker.sh
+++ hacker.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 #(C) Octycat Studios
 
+# CONFIG - START
+
+#Time per next number
+WaitTime=.001  #Default .001
+
+#How many times the numbers print
+Amount=10000  #Default 10000
+
+
+
 clear
 
 RANGE=2
 counter=1
 
-while [[ $counter -le 10000 ]]; do
+while [[ $counter -le $Amount ]]; do
 	#statements
 
 number=$RANDOM
 let "number %= $RANGE"
 echo -n "$number"
-sleep .001
+sleep $WaitTime
 number=$RANDOM
 let "number %= $RANGE"
 echo -n "$number"
